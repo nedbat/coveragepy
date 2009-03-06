1e4a0f0bdb32f2ae041db8e086db75a615192c62
Ned Batchelder <ned@nedbatchelder.com>
2009-03-05 21:38:54 -0500

    Fix up some string continuations.

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 # setup.py for coverage.
 
-"""\
-Code coverage testing for Python
+"""Code coverage testing for Python
 
 Coverage.py is a Python package that measures code coverage during test execution.
 It uses the code analysis tools and tracing hooks provided in the Python standard
 library to determine which lines are executable, and which have been executed.
 """
 
-classifiers = """\
+classifiers = """
 Development Status :: 5 - Production/Stable
 Environment :: Console
 Intended Audience :: Developers
