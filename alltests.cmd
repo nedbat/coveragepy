0c531a17ab71812eaecb03de7c1b8c3ffadcaaae
Ned Batchelder <ned@nedbatchelder.com>
2009-03-14 23:28:28 -0400

    Test the code without a C extension.

--- alltests.cmd
+++ alltests.cmd
@@ -1,12 +1,20 @@
 call \ned\bin\switchpy 23
 python setup.py develop
 nosetests
+del coverage\tracer.pyd
+nosetests
 call \ned\bin\switchpy 24
 python setup.py develop
+nosetests
+del coverage\tracer.pyd
 nosetests
 call \ned\bin\switchpy 25
 python setup.py develop
 nosetests
+del coverage\tracer.pyd
+nosetests
 call \ned\bin\switchpy 26
 python setup.py develop
+nosetests
+del coverage\tracer.pyd
 nosetests
