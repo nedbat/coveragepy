b2dfda7020ecce0e895d471fa305f5f7c64a983c
Ned Batchelder <ned@nedbatchelder.com>
2009-04-18 21:14:49 -0400

    Quieter alltests.cmd

--- alltests.cmd
+++ alltests.cmd
@@ -1,20 +1,20 @@
 call \ned\bin\switchpy 23
-python setup.py develop
+python setup.py -q develop
 nosetests
 del coverage\tracer.pyd
 nosetests
 call \ned\bin\switchpy 24
-python setup.py develop
+python setup.py -q develop
 nosetests
 del coverage\tracer.pyd
 nosetests
 call \ned\bin\switchpy 25
-python setup.py develop
+python setup.py -q develop
 nosetests
 del coverage\tracer.pyd
 nosetests
 call \ned\bin\switchpy 26
-python setup.py develop
+python setup.py -q develop
 nosetests
 del coverage\tracer.pyd
 nosetests
