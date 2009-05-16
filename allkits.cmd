a14ad24a559b175bc109abb1b91aa888d3078bd6
Ned Batchelder <ned@nedbatchelder.com>
2009-05-16 11:21:34 -0400

    No need to build source kits on each Python version.

--- allkits.cmd
+++ allkits.cmd
@@ -1,11 +1,8 @@
 call \ned\bin\switchpy 23
-python setup.py sdist --formats=gztar
 python setup.py bdist_wininst
 call \ned\bin\switchpy 24
-python setup.py sdist --formats=gztar
 python setup.py bdist_wininst
 call \ned\bin\switchpy 25
-python setup.py sdist --formats=gztar
 python setup.py bdist_wininst
 call \ned\bin\switchpy 26
 python setup.py sdist --formats=gztar
