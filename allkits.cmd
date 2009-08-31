2cc824e072170d79b0b7644ac5c9ede6f96a8f94
Ned Batchelder <ned@nedbatchelder.com>
2009-08-30 21:39:41 -0400

    Force a group id of 100 when building the source tar.gz.  Fixes issue #7.

--- allkits.cmd
+++ allkits.cmd
@@ -5,5 +5,7 @@ python setup.py bdist_wininst
 call \ned\bin\switchpy 25
 python setup.py bdist_wininst
 call \ned\bin\switchpy 26
-python setup.py sdist --formats=gztar
 python setup.py bdist_wininst
+set TAR_OPTIONS=--group=100
+python setup.py sdist --formats=gztar
+set TAR_OPTIONS=
