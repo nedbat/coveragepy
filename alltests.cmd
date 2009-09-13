2b9d62727d97dcdd72e88914d66f361372097368
Ned Batchelder <ned@nedbatchelder.com>
2009-09-13 08:19:22 -0400

    Run just specific tests

--- alltests.cmd
+++ alltests.cmd
@@ -1,31 +1,31 @@
 call \ned\bin\switchpy 23
 python setup.py -q develop
 set COVERAGE_TEST_TRACER=c
-nosetests
+nosetests %1
 del coverage\tracer.pyd
 set COVERAGE_TEST_TRACER=py
-nosetests
+nosetests %1
 
 call \ned\bin\switchpy 24
 python setup.py -q develop
 set COVERAGE_TEST_TRACER=c
-nosetests
+nosetests %1
 del coverage\tracer.pyd
 set COVERAGE_TEST_TRACER=py
-nosetests
+nosetests %1
 
 call \ned\bin\switchpy 25
 python setup.py -q develop
 set COVERAGE_TEST_TRACER=c
-nosetests
+nosetests %1
 del coverage\tracer.pyd
 set COVERAGE_TEST_TRACER=py
-nosetests
+nosetests %1
 
 call \ned\bin\switchpy 26
 python setup.py -q develop
 set COVERAGE_TEST_TRACER=c
-nosetests
+nosetests %1
 del coverage\tracer.pyd
 set COVERAGE_TEST_TRACER=py
-nosetests
+nosetests %1
