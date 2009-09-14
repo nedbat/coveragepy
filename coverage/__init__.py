cc4914d27819d859cf6a23a255c55cf6455de64e
Ned Batchelder <ned@nedbatchelder.com>
2009-09-13 20:07:23 -0400

    Bump version number to 3.1b1.

--- coverage/__init__.py
+++ coverage/__init__.py
@@ -5,7 +5,7 @@ http://nedbatchelder.com/code/coverage
 
 """
 
-__version__ = "3.0.1"    # see detailed history in CHANGES.txt
+__version__ = "3.1b1"    # see detailed history in CHANGES.txt
 
 from coverage.control import coverage
 from coverage.data import CoverageData
