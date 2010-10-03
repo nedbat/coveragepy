90cff0263c3838897c0a54f86b6df7613121c457
Ned Batchelder <ned@nedbatchelder.com>
2010-10-03 08:19:54 -0400

    Bump the version number.

--- coverage/__init__.py
+++ coverage/__init__.py
@@ -5,11 +5,11 @@ http://nedbatchelder.com/code/coverage
 
 """
 
-__version__ = "3.4"     # see detailed history in CHANGES.txt
+__version__ = "3.5a1"     # see detailed history in CHANGES.txt
 
 __url__ = "http://nedbatchelder.com/code/coverage"
-if 'b' in __version__:
-    # For beta, use a version-specific URL.
+if max(__version__) in "abc":
+    # For pre-releases, use a version-specific URL.
     __url__ += "/" + __version__
 
 from coverage.control import coverage, process_startup
