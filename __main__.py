40ac04235ae802caa74215157cab3f0b77c48e8d
Ned Batchelder <ned@nedbatchelder.com>
2011-04-16 20:45:34 -0400

    Push some characters around.

--- __main__.py
+++ __main__.py
@@ -1,8 +1,6 @@
-"""Be able to execute coverage.py by pointing Python at the repository's
-directory."""
-import os
-import runpy
+"""Be able to execute coverage.py by pointing Python at a working tree."""
 
+import runpy, os
 
 PKG = 'coverage'
 
@@ -10,7 +8,8 @@ try:
     run_globals = runpy.run_module(PKG, run_name='__main__', alter_sys=True)
     executed = os.path.splitext(os.path.basename(run_globals['__file__']))[0]
     if executed != '__main__':  # For Python 2.5 compatibility
-        raise ImportError('Incorrectly executed %s instead of __main__' %
-                            executed)
+        raise ImportError(
+            'Incorrectly executed %s instead of __main__' % executed
+            )
 except ImportError:  # For Python 2.6 compatibility
     runpy.run_module('%s.__main__' % PKG, run_name='__main__', alter_sys=True)
