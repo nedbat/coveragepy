3a67982794e40043e51449e1d40b07258ef8b932
Ned Batchelder <ned@nedbatchelder.com>
2012-05-11 23:00:34 -0400

    Add a test case for bug #179.

--- test/farm/html/run_bom.py
+++ test/farm/html/run_bom.py
@@ -1,21 +1,21 @@
 import sys
 
 def html_it():
-    """Run coverage and make an HTML report for isolatin1.py."""
+    """Run coverage and make an HTML report for bom.py."""
     import coverage
     cov = coverage.coverage()
     cov.start()
-    import isolatin1
+    import bom
     cov.stop()
-    cov.html_report(isolatin1, directory="../html_isolatin1")
+    cov.html_report(bom, directory="../html_bom")
 
 runfunc(html_it, rundir="src")
 
 # HTML files will change often.  Check that the sizes are reasonable,
 #   and check that certain key strings are in the output.
-compare("gold_isolatin1", "html_isolatin1", size_within=10, file_pattern="*.html")
-contains("html_isolatin1/isolatin1.html",
+compare("gold_bom", "html_bom", size_within=10, file_pattern="*.html")
+contains("html_bom/bom.html",
     "<span class='str'>&quot;3&#215;4 = 12, &#247;2 = 6&#177;0&quot;</span>",
     )
 
-clean("html_isolatin1")
+clean("html_bom")
---
+++ test/farm/html/src/bom.py
@@ -0,0 +1,11 @@
+﻿# A python source file in utf-8, with BOM
+math = "3×4 = 12, ÷2 = 6±0"
+
+import sys
+
+if sys.version_info >= (3, 0):
+    assert len(math) == 18
+    assert len(math.encode('utf-8')) == 21
+else:
+    assert len(math) == 21
+    assert len(math.decode('utf-8')) == 18
