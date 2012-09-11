a65ce0146e0fdbb1ea08a4e12e47f2a7c11427f5
Ned Batchelder <ned@nedbatchelder.com>
2012-09-10 21:10:27 -0400

    It's 2012 already.

--- coverage/__init__.py
+++ coverage/__init__.py
@@ -60,7 +60,7 @@ annotate =  _singleton_method('annotate')
 # COPYRIGHT AND LICENSE
 #
 # Copyright 2001 Gareth Rees.  All rights reserved.
-# Copyright 2004-2011 Ned Batchelder.  All rights reserved.
+# Copyright 2004-2012 Ned Batchelder.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
