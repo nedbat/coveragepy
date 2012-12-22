e1528a728c830672c3c1a857484d60ae96a68311
Ned Batchelder <ned@nedbatchelder.com>
2012-12-22 09:53:08 -0500

    One more 2.3 thing.

--- coverage/misc.py
+++ coverage/misc.py
@@ -57,7 +57,7 @@ def format_lines(statements, lines):
 def short_stack():
     """Return a string summarizing the call stack."""
     stack = inspect.stack()[:0:-1]
-    return "\n".join("%30s : %s @%d" % (t[3],t[1],t[2]) for t in stack)
+    return "\n".join(["%30s : %s @%d" % (t[3],t[1],t[2]) for t in stack])
 
 
 def expensive(fn):
