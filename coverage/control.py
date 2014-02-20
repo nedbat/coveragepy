4e1a87c430f9776fa0d4f0a1655f052a097859cd
Chris Rose <chrisros@amazon.com>
2014-02-19 19:59:44 -0800

    Clean up the comment on the xmlreport fix

--- coverage/control.py
+++ coverage/control.py
@@ -692,11 +692,10 @@ class coverage(object):
             if self.config.xml_output == '-':
                 outfile = sys.stdout
             else:
-                # ensure that the output directory is created; done here
-                # because this report pre-opens the output
-                # file. HTMLReport does this using the Report plumbing
-                # because its task is more complex, being multiple
-                # files.
+                # Ensure that the output directory is created; done here
+                # because this report pre-opens the output file.
+                # HTMLReport does this using the Report plumbing because
+                # its task is more complex, being multiple files.
                 output_dir = os.path.dirname(self.config.xml_output)
                 if output_dir and not os.path.isdir(output_dir):
                     os.makedirs(output_dir)
