460d303ed2361abe554da075c184a370d7d54701
Anthony Sottile <asottile@umich.edu>
2014-06-29 14:35:02 -0700

    Fix non-comment encoding detection.
    --HG--
    branch : fix_source_encoding

--- tests/test_phystokens.py
+++ tests/test_phystokens.py
@@ -97,6 +97,11 @@ if sys.version_info < (3, 0):
             source = "# This Python file uses this encoding: utf-8\n"
             self.assertEqual(source_encoding(source), 'utf-8')
 
+        def test_detect_source_encoding_not_in_comment(self):
+            # Should not detect anything here
+            source = 'def parse(src, encoding=None):\n    pass'
+            self.assertEqual(source_encoding(source), 'ascii')
+
         def test_detect_source_encoding_on_second_line(self):
             # A coding declaration should be found despite a first blank line.
             source = "\n# coding=cp850\n\n"
