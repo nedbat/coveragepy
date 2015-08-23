57e59d2cb3d7d6a793194806ddea039d73442e9d
Ned Batchelder <ned@nedbatchelder.com>
2015-08-23 15:18:38 -0400

    Clean up test_osinfo.py

--- tests/osinfo.py
+++ tests/osinfo.py
@@ -11,29 +11,29 @@ if env.WINDOWS:
     def process_ram():
         """How much RAM is this process using? (Windows)"""
         import ctypes
-        # lifted from:
-        # lists.ubuntu.com/archives/bazaar-commits/2009-February/011990.html
+        # From: http://lists.ubuntu.com/archives/bazaar-commits/2009-February/011990.html
         class PROCESS_MEMORY_COUNTERS_EX(ctypes.Structure):
             """Used by GetProcessMemoryInfo"""
-            _fields_ = [('cb', ctypes.c_ulong),
-                        ('PageFaultCount', ctypes.c_ulong),
-                        ('PeakWorkingSetSize', ctypes.c_size_t),
-                        ('WorkingSetSize', ctypes.c_size_t),
-                        ('QuotaPeakPagedPoolUsage', ctypes.c_size_t),
-                        ('QuotaPagedPoolUsage', ctypes.c_size_t),
-                        ('QuotaPeakNonPagedPoolUsage', ctypes.c_size_t),
-                        ('QuotaNonPagedPoolUsage', ctypes.c_size_t),
-                        ('PagefileUsage', ctypes.c_size_t),
-                        ('PeakPagefileUsage', ctypes.c_size_t),
-                        ('PrivateUsage', ctypes.c_size_t),
-                       ]
+            _fields_ = [
+                ('cb', ctypes.c_ulong),
+                ('PageFaultCount', ctypes.c_ulong),
+                ('PeakWorkingSetSize', ctypes.c_size_t),
+                ('WorkingSetSize', ctypes.c_size_t),
+                ('QuotaPeakPagedPoolUsage', ctypes.c_size_t),
+                ('QuotaPagedPoolUsage', ctypes.c_size_t),
+                ('QuotaPeakNonPagedPoolUsage', ctypes.c_size_t),
+                ('QuotaNonPagedPoolUsage', ctypes.c_size_t),
+                ('PagefileUsage', ctypes.c_size_t),
+                ('PeakPagefileUsage', ctypes.c_size_t),
+                ('PrivateUsage', ctypes.c_size_t),
+            ]
 
         mem_struct = PROCESS_MEMORY_COUNTERS_EX()
         ret = ctypes.windll.psapi.GetProcessMemoryInfo(
-                    ctypes.windll.kernel32.GetCurrentProcess(),
-                    ctypes.byref(mem_struct),
-                    ctypes.sizeof(mem_struct)
-                    )
+            ctypes.windll.kernel32.GetCurrentProcess(),
+            ctypes.byref(mem_struct),
+            ctypes.sizeof(mem_struct)
+        )
         if not ret:
             return 0
         return mem_struct.PrivateUsage
@@ -47,17 +47,17 @@ elif env.LINUX:
     def _VmB(key):
         """Read the /proc/PID/status file to find memory use."""
         try:
-            # get pseudo file /proc/<pid>/status
+            # Get pseudo file /proc/<pid>/status
             with open('/proc/%d/status' % os.getpid()) as t:
                 v = t.read()
         except IOError:
             return 0    # non-Linux?
-         # get VmKey line e.g. 'VmRSS:  9999  kB\n ...'
+        # Get VmKey line e.g. 'VmRSS:  9999  kB\n ...'
         i = v.index(key)
         v = v[i:].split(None, 3)
         if len(v) < 3:
-            return 0    # invalid format?
-         # convert Vm value to bytes
+            return 0    # Invalid format?
+        # Convert Vm value to bytes.
         return int(float(v[1]) * _scale[v[2].lower()])
 
     def process_ram():
