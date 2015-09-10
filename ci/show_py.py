409ffb2aaa842867950722602126e4f335fc9691
Ned Batchelder <ned@nedbatchelder.com>
2015-09-10 08:02:39 -0400

    This is like building a ship in a bottle

--- ci/show_py.py
+++ ci/show_py.py
@@ -4,25 +4,37 @@ import sys
 
 
 def main():
+    show_version()
+    show_sizes()
+    show_encoding()
+    show_path()
+
+
+def show_version():
     print("Python version:\n    {0}".format(sys.version.replace("\n", "\n    ")))
-    print("Python prefix: {0}".format(sys.prefix))
+    print("Python executable: {0!r}".format(sys.executable))
+    print("Python prefix: {0!r}".format(sys.prefix))
     if hasattr(sys, "real_prefix"):
-        print("This is a virtualenv.  The real prefix is: {0}".format(sys.real_prefix))
+        print("This is a virtualenv.  The real prefix is: {0!r}".format(sys.real_prefix))
     else:
         print("This is not a virtualenv.")
 
+
+def show_sizes():
     if sys.maxsize == 2**63-1:
         indicates = "indicating 64-bit"
     elif sys.maxsize == 2**31-1:
         indicates = "indicating 32-bit"
     else:
         indicates = "not sure what that means"
-    print("sys.maxsize: {0}, {1}".format(sys.maxsize, indicates))
+    print("sys.maxsize: {0!r}, {1}".format(sys.maxsize, indicates))
     if hasattr(sys, "maxint"):
-        print("sys.maxint: {0}".format(sys.maxint))
+        print("sys.maxint: {0!r}".format(sys.maxint))
     else:
         print("sys.maxint doesn't exist")
 
+
+def show_encoding():
     if sys.version_info < (3, 0):
         if sys.maxunicode == 1114111:
             indicates = "indicating a wide Unicode build"
@@ -30,10 +42,15 @@ def main():
             indicates = "indicating a narrow Unicode build"
     else:
         indicates = "as all Python 3 have"
-    print("sys.maxunicode: {0}, {1}".format(sys.maxunicode, indicates))
+    print("sys.maxunicode: {0!r}, {1}".format(sys.maxunicode, indicates))
 
-    print("sys.getdefaultencoding(): {0}".format(sys.getdefaultencoding()))
-    print("sys.getfilesystemencoding(): {0}".format(sys.getfilesystemencoding()))
+    print("sys.getdefaultencoding(): {0!r}".format(sys.getdefaultencoding()))
+    print("sys.getfilesystemencoding(): {0!r}".format(sys.getfilesystemencoding()))
+
+
+def show_path():
+    print("sys.path:")
+    print("\n".join("    {0!r}".format(p) for p in sys.path))
 
 
 if __name__ == "__main__":
