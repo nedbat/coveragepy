c5d55e387e360f98268f40e557780dbd1a73e328
Ned Batchelder <ned@nedbatchelder.com>
2015-11-27 06:50:42 -0500

    Actually, 4.1

--- coverage/version.py
+++ coverage/version.py
@@ -5,7 +5,7 @@
 # This file is exec'ed in setup.py, don't import anything!
 
 # Same semantics as sys.version_info.
-version_info = (4, 0, 4, 'final', 0)
+version_info = (4, 1, 0, 'alpha', 0)
 
 
 def _make_version(major, minor, micro, releaselevel, serial):
