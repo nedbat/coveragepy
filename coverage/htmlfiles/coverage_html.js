e19650faa42342eeabb111ca08a531754c03c58a
Ned Batchelder <ned@nedbatchelder.com>
2016-01-31 06:46:15 -0500

    Formatting fixes

--- coverage/htmlfiles/coverage_html.js
+++ coverage/htmlfiles/coverage_html.js
@@ -513,7 +513,7 @@ coverage.finish_scrolling = function () {
     $("html,body").stop(true, true);
 };
 
-coverage.init_scroll_markers = function(){
+coverage.init_scroll_markers = function () {
     var c = coverage;
     // Init some variables
     c.lines_len = $('td.text p').length;
@@ -523,18 +523,17 @@ coverage.init_scroll_markers = function(){
 
     // Build html
     c.resize_scroll_markers();
+};
 
-}
-
-coverage.resize_scroll_markers = function(){
+coverage.resize_scroll_markers = function () {
     var c = coverage,
-        min_height = 3
+        min_height = 3,
         max_height = 10,
         previous_line = 0,
         visible_window_h = $(window).height();
     $('#scroll_marker').remove();
     // Don't build markers if window hasn't scroll
-    if (c.body_h<=visible_window_h){
+    if (c.body_h<=visible_window_h) {
         return false;
     }
 
@@ -544,42 +543,44 @@ coverage.resize_scroll_markers = function(){
         'height':visible_window_h+'px'
     });
     var header_amend = Math.round(c.header_h*scroll_marker.height()/c.body_h),
-        line_height =  scroll_marker.height()/c.lines_len,
+        line_height = scroll_marker.height()/c.lines_len,
         general_height = 0;
 
     // Line height must be between the extremums
-    if (line_height>min_height){
-        if (line_height>max_height){
-            line_height= max_height
+    if (line_height > min_height) {
+        if (line_height > max_height) {
+            line_height = max_height;
         }
-    }else{
-        line_height = min_height
+    }
+    else{
+        line_height = min_height;
     }
 
-    c.missed_lines.each(function(){
-        var line_position = Math.round($(this).offset().top*scroll_marker.height()/c.body_h)
+    c.missed_lines.each(function () {
+        var line_position = Math.round($(this).offset().top*scroll_marker.height()/c.body_h);
         var id_name = $(this).attr('id'),
             line_number = id_name.substring(1, id_name.length);
-        if(line_number== parseInt(previous_line)+1){
+        if (line_number == parseInt(previous_line)+1) {
             // If this solid missed block just make previous line higher
             $('#m' + previous_line).attr('id','m' + line_number).css({
                 'height': "+=" + line_height
             });
-        }else {
+        }
+        else {
             // Add colored line in scroll_marker block
             scroll_marker.append('<div id="m' + line_number + '" class="marker"></div>');
             $('#m' + line_number).css({
                 'height': line_height ,
                 'top': line_position- header_amend-general_height
-            })
+            });
         }
         // Counters
         general_height += line_height;
         previous_line = line_number;
     });
-}
+};
 
-$(window).resize(function(){
+$(window).resize(function () {
     // Rebuild scroll markers after window high changing
-    coverage.resize_scroll_markers()
+    coverage.resize_scroll_markers();
 });
