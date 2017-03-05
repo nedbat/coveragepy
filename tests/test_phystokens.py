d150179c75475d12a9c6ce01564a2ba9ea474d93
Ned Batchelder <ned@nedbatchelder.com>
2017-03-04 19:56:33 -0500

    Describe and relabel a test that has nothing to do with fail-under.

--- tests/test_phystokens.py
+++ tests/test_phystokens.py
@@ -228,11 +228,15 @@ class Bug529Test(CoverageTest):
     """Test of bug 529"""
 
     def test_bug_529(self):
+        # Don't over-neuter coding declarations. This happened with a test
+        # file which contained code in multi-line strings, all with coding
+        # declarations. The neutering of the file also changed the multi-line
+        # strings, which it shouldn't have.
         self.make_file("the_test.py", '''\
             # -*- coding: utf-8 -*-
             import unittest
-            class FailsUnderCoverageTest(unittest.TestCase):
-                def test_fails_under_coverage(self):
+            class Bug529Test(unittest.TestCase):
+                def test_two_strings_are_equal(self):
                     src1 = u"""\\
                         # -*- coding: utf-8 -*-
                         # Just a comment.
