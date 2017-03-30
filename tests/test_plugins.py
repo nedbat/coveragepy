b81ed16b9af2743a21ebea6344c43f72ec16c61e
Ned Batchelder <ned@nedbatchelder.com>
2017-03-30 10:46:09 -0400

    Windows has a different error message for path(float)

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -707,6 +707,7 @@ class BadPluginTest(FileTracerTest):
             excmsgs=[
                 "expected str, bytes or os.PathLike object, not float",
                 "'float' object has no attribute",
+                "object of type 'float' has no len()",
             ],
         )
 
