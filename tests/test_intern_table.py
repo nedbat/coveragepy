649fb2a310f1aa8a4404eb82f729c1ffb0f742f7
David R. MacIver <david@drmaciver.com>
2017-09-25 17:13:17 +0100

    Add a test that we're managing refcounts correctly

--- tests/test_intern_table.py
+++ tests/test_intern_table.py
@@ -3,6 +3,7 @@ from tests.coveragetest import CoverageTest
 from hypothesis import given, example
 import hypothesis.strategies as st
 from coverage.tracer import InternTable
+import sys
 
 
 class TestInternTable(CoverageTest):
@@ -27,3 +28,29 @@ class TestInternTable(CoverageTest):
         for i in ls:
             assert table[i] == i
         assert len(table) == len(ls)
+
+    @given(
+        st.lists(st.tuples(st.integers(0, 2**64 - 1)), unique=True).map(
+            lambda ls: list(map(list, ls))
+        ),
+    )
+    def test_maintains_correct_ref_count(self, ls):
+        def refs():
+            return list(map(sys.getrefcount, ls))
+
+        initial_refs = refs()
+        assert refs() == initial_refs
+
+        table = InternTable()
+
+        for i in range(len(ls)):
+            r = initial_refs[i]
+            assert sys.getrefcount(ls[i]) == r + 1
+            x = ls[i][0]
+            table[x] = ls[i]
+            assert sys.getrefcount(ls[i]) == r + 2
+            table[x] = ls[i]
+            assert sys.getrefcount(ls[i]) == r + 2
+
+        del table
+        assert refs() == initial_refs
