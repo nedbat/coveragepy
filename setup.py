5bf4282268ba2f4b0a5f7bd5ace34b24d6a12d9c
Jon Dufresne <jon.dufresne@gmail.com>
2017-10-21 10:25:13 -0700

    Add missing classifiers for supported Python versions
    Documents support for general Python, Python 2, & Python 3 support.

--- setup.py
+++ setup.py
@@ -22,8 +22,11 @@ Environment :: Console
 Intended Audience :: Developers
 License :: OSI Approved :: Apache Software License
 Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 2
 Programming Language :: Python :: 2.6
 Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
 Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
