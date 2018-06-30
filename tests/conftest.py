9f6284eea50a56efe6125ff4b760e72cbc445244
Ned Batchelder <ned@nedbatchelder.com>
2018-06-29 22:07:46 -0400

    Suppress needless warnings during tests
    (cherry picked from commit 9e783c683cf2013730a84c6f5e2e7f4439d7a352)

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,9 +10,23 @@ This module is run automatically by pytest, to define and enable fixtures.
 import pytest
 import warnings
 
+from coverage import env
+
 
 @pytest.fixture(autouse=True)
 def set_warnings():
     """Enable DeprecationWarnings during all tests."""
     warnings.simplefilter("default")
     warnings.simplefilter("once", DeprecationWarning)
+
+    # A warning to suppress:
+    #   setuptools/py33compat.py:54: DeprecationWarning: The value of convert_charrefs will become
+    #   True in 3.5. You are encouraged to set the value explicitly.
+    #       unescape = getattr(html, 'unescape', html_parser.HTMLParser().unescape)
+    warnings.filterwarnings(
+        "ignore",
+        category=DeprecationWarning,
+        message="The value of convert_charrefs will become True in 3.5.",
+        )
+    if env.PYPY:
+        warnings.filterwarnings("ignore", r".*unclosed file", category=ResourceWarning)
