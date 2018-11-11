ef6db13296f8450e30601490ee33a9643c9db0cf
Ned Batchelder <ned@nedbatchelder.com>
2018-11-11 07:59:54 -0500

    One last decorator test change for 3.8

--- tests/test_arcs.py
+++ tests/test_arcs.py
@@ -1372,6 +1372,10 @@ class DecoratorArcTest(CoverageTest):
     def test_bug_466(self):
         # A bad interaction between decorators and multi-line list assignments,
         # believe it or not...!
+        if env.PYBEHAVIOR.trace_decorated_def:
+            arcz = ".1 1A A.  13 34 4.  -35 58 8-3"
+        else:
+            arcz = ".1 1A A.  13 3.     -35 58 8-3"
         self.check_coverage("""\
             class Parser(object):
 
@@ -1384,8 +1388,12 @@ class DecoratorArcTest(CoverageTest):
 
             Parser.parse()
             """,
-            arcz=".1 1A A.  13 3.  -35 58 8-3",
+            arcz=arcz,
         )
+        if env.PYBEHAVIOR.trace_decorated_def:
+            arcz = ".1 1A A.  13 34 4.  -35 58 8-3"
+        else:
+            arcz = ".1 1A A.  13 3.     -35 58 8-3"
         self.check_coverage("""\
             class Parser(object):
 
@@ -1398,7 +1406,7 @@ class DecoratorArcTest(CoverageTest):
 
             Parser.parse()
             """,
-            arcz=".1 1A A.  13 3.  -35 58 8-3",
+            arcz=arcz,
         )
 
 
