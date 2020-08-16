aa6cb95e366f479cddd8cd30aee940393ead6296
Ned Batchelder <ned@nedbatchelder.com>
2020-08-16 12:58:05 -0400

    Don't build the help includes into HTML

--- doc/conf.py
+++ doc/conf.py
@@ -97,7 +97,7 @@ rst_epilog = """
 
 # List of directories, relative to source directory, that shouldn't be searched
 # for source files.
-exclude_trees = ['_build']
+exclude_patterns = ["_build", "help/*"]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
