5f7d6c4272143b6e860074422c4e89390684807e
Ned Batchelder <ned@nedbatchelder.com>
2021-04-14 15:23:41 -0400

    build: suppress new 3.10 warnings

--- tests/conftest.py
+++ tests/conftest.py
@@ -56,6 +56,17 @@ def set_warnings():
         category=ImportWarning,
         message=r".*exec_module\(\) not found; falling back to load_module\(\)",
         )
+    # <frozen importlib._bootstrap>:908:
+    # ImportWarning: AssertionRewritingHook.find_spec() not found; falling back to find_module()
+    # <frozen importlib._bootstrap>:908:
+    # ImportWarning: _SixMetaPathImporter.find_spec() not found; falling back to find_module()
+    # <frozen importlib._bootstrap>:908:
+    # ImportWarning: VendorImporter.find_spec() not found; falling back to find_module()
+    warnings.filterwarnings(
+        "ignore",
+        category=ImportWarning,
+        message=r".*find_spec\(\) not found; falling back to find_module\(\)",
+        )
 
     if env.PYPY3:
         # pypy3 warns about unclosed files a lot.
