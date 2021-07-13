caf7639d9a74d8adda15ad08fa1201c47efba86b
Ned Batchelder <ned@nedbatchelder.com>
2021-07-13 07:34:46 -0400

    test: the code I use for bpo reports

---
+++ lab/bpo_prelude.py
@@ -0,0 +1,12 @@
+import linecache, sys
+
+def trace(frame, event, arg):
+    # The weird globals here is to avoid a NameError on shutdown...
+    if frame.f_code.co_filename == globals().get("__file__"):
+        lineno = frame.f_lineno
+        print("{} {}: {}".format(event[:4], lineno, linecache.getline(__file__, lineno).rstrip()))
+    return trace
+
+print(sys.version)
+sys.settrace(trace)
+
