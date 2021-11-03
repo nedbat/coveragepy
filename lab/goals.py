311d16a4fe6274dedcf0ade8a68e1b010833a341
Ned Batchelder <ned@nedbatchelder.com>
2021-11-03 07:23:23 -0400

    docs: add a license notice to the top of goals.py

--- lab/goals.py
+++ lab/goals.py
@@ -1,3 +1,6 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://github.com/nedbat/coveragepy/blob/master/NOTICE.txt
+
 """\
 Check coverage goals.
 
