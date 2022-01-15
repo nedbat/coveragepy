4eac4a51c089d90478c81a5e7b483744ef0aa4fe
Ned Batchelder <ned@nedbatchelder.com>
2022-01-15 14:01:08 -0500

    test: slightly nicer bpo prelude

--- lab/bpo_prelude.py
+++ lab/bpo_prelude.py
@@ -4,7 +4,8 @@ def trace(frame, event, arg):
     # The weird globals here is to avoid a NameError on shutdown...
     if frame.f_code.co_filename == globals().get("__file__"):
         lineno = frame.f_lineno
-        print("{} {}: {}".format(event[:4], lineno, linecache.getline(__file__, lineno).rstrip()))
+        line = linecache.getline(__file__, lineno).rstrip()
+        print("{} {}: {}".format(event[:4], lineno, line))
     return trace
 
 print(sys.version)
