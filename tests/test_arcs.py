2fd0dc69e27e083b4e68b86e52ddc78cf66e56bf
Ned Batchelder <ned@nedbatchelder.com>
2022-01-19 20:03:25 -0500

    test: i can't figure out what versions of 3.9 this passes or fails

--- tests/test_arcs.py
+++ tests/test_arcs.py
@@ -1942,7 +1942,7 @@ class AsyncTest(CoverageTest):
 
     # https://github.com/nedbat/coveragepy/issues/1158
     # https://bugs.python.org/issue44621
-    @pytest.mark.xfail(env.PYVERSION[:2] == (3, 9), reason="avoid a 3.9 bug: 44621")
+    @pytest.mark.skipif(env.PYVERSION[:2] == (3, 9), reason="avoid a 3.9 bug: 44621")
     @pytest.mark.skipif(env.PYVERSION < (3, 7), reason="need asyncio.run")
     def test_bug_1158(self):
         self.check_coverage("""\
