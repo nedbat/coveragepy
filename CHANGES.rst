efbda9ece633d8a97f950bb09d3324b4f458ffec
Ned Batchelder <ned@nedbatchelder.com>
2022-12-23 18:21:31 -0500

    docs: bump version

--- CHANGES.rst
+++ CHANGES.rst
@@ -17,6 +17,12 @@ development at the same time, such as 4.5.x and 5.0.
     ..  Version 9.8.1 — 2027-07-27
     ..  --------------------------
 
+Unreleased
+----------
+
+Nothing yet.
+
+
 .. _changes_7-0-1:
 
 Version 7.0.1 — 2022-12-23
--- coverage/version.py
+++ coverage/version.py
@@ -6,8 +6,8 @@
 
 # version_info: same semantics as sys.version_info.
 # _dev: the .devN suffix if any.
-version_info = (7, 0, 1, "final", 0)
-_dev = 0
+version_info = (7, 0, 2, "alpha", 0)
+_dev = 1
 
 
 def _make_version(major, minor, micro, releaselevel="final", serial=0, dev=0):
