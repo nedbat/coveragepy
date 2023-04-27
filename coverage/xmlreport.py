nedbat/coveragepy

commit 45cf7936ee605cfe06f7f5967a72a73198960120

fix: more relative_files=true fixes. #1280

coverage/xmlreport.py
@@ -149,7 +149,8 @@ class XmlReporter:
         # are populated later.  Note that a package == a directory.
         filename = fr.filename.replace("\\", "/")
         for source_path in self.source_paths:
-            source_path = files.canonical_filename(source_path)
+            if not self.config.relative_files:
+                source_path = files.canonical_filename(source_path)
             if filename.startswith(source_path.replace("\\", "/") + "/"):
                 rel_name = filename[len(source_path)+1:]
                 break

coverage/python.py
@@ -151,7 +151,14 @@ class PythonFileReporter(FileReporter):
 
         filename = source_for_morf(morf)
 
-        super().__init__(canonical_filename(filename))
+        fname = filename
+        canonicalize = True
+        if self.coverage is not None:
+            if self.coverage.config.relative_files:
+                canonicalize = False
+        if canonicalize:
+            fname = canonical_filename(filename)
+        super().__init__(fname)
 
         if hasattr(morf, '__name__'):
             name = morf.__name__.replace(".", os.sep)

tests/test_api.py
@@ -1239,6 +1239,38 @@ class RelativePathTest(CoverageTest):
         self.assert_file_count(".coverage.*", 0)
         self.assert_exists(".coverage")
 
+    def test_files_up_one_level(self):
+        # https://github.com/nedbat/coveragepy/issues/1280
+        self.make_file("src/mycode.py", """\
+            def foo():
+                return 17
+            """)
+        self.make_file("test/test_it.py", """\
+            from src.mycode import foo
+            assert foo() == 17
+            """)
+        self.make_file("test/.coveragerc", """\
+            [run]
+            parallel = True
+            relative_files = True
+
+            [paths]
+            source =
+                ../src/
+                */src
+            """)
+        os.chdir("test")
+        sys.path.insert(0, "..")
+        cov1 = coverage.Coverage()
+        self.start_import_stop(cov1, "test_it")
+        cov1.save()
+        cov2 = coverage.Coverage()
+        cov2.combine()
+        cov3 = coverage.Coverage()
+        cov3.load()
+        report = self.get_report(cov3)
+        assert self.last_line_squeezed(report) == "TOTAL 4 0 100%"
+
 
 class CombiningTest(CoverageTest):
     """More tests of combining data."""

commit 4104428fa5de59c3459b695e764f3018735f97db

Strip trailing slash for relative source paths
This fixes an issue introduced in 45cf7936ee605cfe06f7f5967a72a73198960120
where using `relative_files=True` and `src` with a trailing slash,
the source inserted as `<sources>` in the XML report would also have
a trailing slash. This also fixes an issue introduced in the same
commit where an empty `<source>` would be inserted as well for cases
where the `src` has a trailing slash.

coverage/xmlreport.py
@@ -69,6 +69,8 @@ class XmlReporter:
                 if os.path.exists(src):
                     if not self.config.relative_files:
                         src = files.canonical_filename(src)
+                    else:
+                        src = src.rstrip(r"\/")
                     self.source_paths.add(src)
         self.packages: Dict[str, PackageData] = {}
         self.xml_out: xml.dom.minidom.Document