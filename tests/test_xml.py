nedbat/coveragepy

commit 45cf7936ee605cfe06f7f5967a72a73198960120

fix: more relative_files=true fixes. #1280

coverage/xmlreport.py
@@ -149,7 +149,8 @@ class XmlReporter:
         # are populated later.  Note that a package == a directory.
         filename = fr.filename.replace("\\", "/")
         for source_path in self.source_paths:
-            source_path = files.canonical_filename(source_path)
+            if not self.config.relative_files:
+                source_path = files.canonical_filename(source_path)
             if filename.startswith(source_path.replace("\\", "/") + "/"):
                 rel_name = filename[len(source_path)+1:]
                 break

coverage/python.py
@@ -151,7 +151,14 @@ class PythonFileReporter(FileReporter):
 
         filename = source_for_morf(morf)
 
-        super().__init__(canonical_filename(filename))
+        fname = filename
+        canonicalize = True
+        if self.coverage is not None:
+            if self.coverage.config.relative_files:
+                canonicalize = False
+        if canonicalize:
+            fname = canonical_filename(filename)
+        super().__init__(fname)
 
         if hasattr(morf, '__name__'):
             name = morf.__name__.replace(".", os.sep)

tests/test_api.py
@@ -1239,6 +1239,38 @@ class RelativePathTest(CoverageTest):
         self.assert_file_count(".coverage.*", 0)
         self.assert_exists(".coverage")
 
+    def test_files_up_one_level(self):
+        # https://github.com/nedbat/coveragepy/issues/1280
+        self.make_file("src/mycode.py", """\
+            def foo():
+                return 17
+            """)
+        self.make_file("test/test_it.py", """\
+            from src.mycode import foo
+            assert foo() == 17
+            """)
+        self.make_file("test/.coveragerc", """\
+            [run]
+            parallel = True
+            relative_files = True
+
+            [paths]
+            source =
+                ../src/
+                */src
+            """)
+        os.chdir("test")
+        sys.path.insert(0, "..")
+        cov1 = coverage.Coverage()
+        self.start_import_stop(cov1, "test_it")
+        cov1.save()
+        cov2 = coverage.Coverage()
+        cov2.combine()
+        cov3 = coverage.Coverage()
+        cov3.load()
+        report = self.get_report(cov3)
+        assert self.last_line_squeezed(report) == "TOTAL 4 0 100%"
+
 
 class CombiningTest(CoverageTest):
     """More tests of combining data."""

commit df1bf082f242cccdcb342000525bede537b95935

fix: source paths with trailing slashes causing inconsistent sources in XML report with relative_paths (#1608)
* Added failing test for source with trailing slash

This test is nearly identical to the one above it, with the only
change being the source that is used. This may end up turning into
a fixture instead if the tests end up being identical after the
fix is made.

* Strip trailing slash for relative source paths

This fixes an issue introduced in 45cf7936ee605cfe06f7f5967a72a73198960120
where using `relative_files=True` and `src` with a trailing slash,
the source inserted as `<sources>` in the XML report would also have
a trailing slash. This also fixes an issue introduced in the same
commit where an empty `<source>` would be inserted as well for cases
where the `src` has a trailing slash.

coverage/xmlreport.py
@@ -69,6 +69,8 @@ class XmlReporter:
                 if os.path.exists(src):
                     if not self.config.relative_files:
                         src = files.canonical_filename(src)
+                    else:
+                        src = src.rstrip(r"\/")
                     self.source_paths.add(src)
         self.packages: Dict[str, PackageData] = {}
         self.xml_out: xml.dom.minidom.Document

tests/test_xml.py
@@ -320,7 +320,7 @@ class XmlReportTest(XmlTestHelpers, CoverageTest):
 
     def test_no_duplicate_packages(self) -> None:
         self.make_file(
-            "namespace/package/__init__.py", 
+            "namespace/package/__init__.py",
             "from . import sample; from . import test; from .subpackage import test"
         )
         self.make_file("namespace/package/sample.py", "print('package.sample')")
@@ -479,6 +479,19 @@ class XmlPackageStructureTest(XmlTestHelpers, CoverageTest):
     def test_relative_source(self) -> None:
         self.make_file("src/mod.py", "print(17)")
         cov = coverage.Coverage(source=["src"])
+        cov.set_option("run:relative_files", True)
+        self.start_import_stop(cov, "mod", modfile="src/mod.py")
+        cov.xml_report()
+
+        with open("coverage.xml") as x:
+            print(x.read())
+        dom = ElementTree.parse("coverage.xml")
+        elts = dom.findall(".//sources/source")
+        assert [elt.text for elt in elts] == ["src"]
+
+    def test_relative_source_trailing_slash(self) -> None:
+        self.make_file("src/mod.py", "print(17)")
+        cov = coverage.Coverage(source=["src/"])
         cov.set_option("run:relative_files", True)
         self.start_import_stop(cov, "mod", modfile="src/mod.py")
         cov.xml_report()