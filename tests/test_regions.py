68c5f930d89c2a10db68b03e0309b59e2c80f9f4
Ned Batchelder <ned@nedbatchelder.com>
2024-04-03 07:27:21 -0400

    feat: region-finding support for functions and classes

---
+++ tests/test_regions.py
@@ -0,0 +1,119 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://github.com/nedbat/coveragepy/blob/master/NOTICE.txt
+
+"""Tests for coverage/regions.py."""
+
+from __future__ import annotations
+
+import collections
+import textwrap
+from pathlib import Path
+
+import pytest
+
+import coverage
+from coverage import env
+from coverage.regions import code_regions
+
+
+skip_pypy38 = pytest.mark.skipif(
+    env.PYPY and env.PYVERSION < (3, 9),
+    reason="PyPy 3.8 somehow gets different results from ast?",
+    # But PyPy 3.8 is almost out of support so meh.
+)
+
+@skip_pypy38
+def test_code_regions() -> None:
+    regions = code_regions(textwrap.dedent("""\
+        # Numbers in this code are the line number.
+        '''Module docstring'''
+
+        CONST = 4
+        class MyClass:
+            class_attr = 6
+
+            def __init__(self):
+                self.x = 9
+
+            def method_a(self):
+                self.x = 12
+                def inmethod():
+                    self.x = 14
+                    class DeepInside:
+                        def method_b():
+                            self.x = 17
+                        class EvenDeeper:
+                            def method_bb():
+                                self.x = 20
+                self.y = 21
+
+            class InnerClass:
+                constant = 24
+                def method_c(self):
+                    self.x = 26
+
+        def func():
+            x = 29
+            y = 30
+            def inner():
+                z = 32
+                def inner_inner():
+                    w = 34
+
+            class InsideFunc:
+                def method_d(self):
+                    self.x = 38
+
+            return 40
+
+        async def afunc():
+            x = 43
+    """))
+
+    assert regions == {
+        "function": {
+            "MyClass.InnerClass.method_c": {26},
+            "MyClass.__init__": {9},
+            "MyClass.method_a": {12, 13, 21},
+            "MyClass.method_a.inmethod": {14, 15, 16, 18, 19},
+            "MyClass.method_a.inmethod.DeepInside.method_b": {17},
+            "MyClass.method_a.inmethod.DeepInside.EvenDeeper.method_bb": {20},
+            "afunc": {43},
+            "func": {29, 30, 31, 35, 36, 37, 39, 40},
+            "func.InsideFunc.method_d": {38},
+            "func.inner": {32, 33},
+            "func.inner.inner_inner": {34},
+        },
+        "class": {
+            "MyClass": {9, 12, 13, 14, 15, 16, 18, 19, 21},
+            "MyClass.InnerClass": {26},
+            "MyClass.method_a.inmethod.DeepInside": {17},
+            "MyClass.method_a.inmethod.DeepInside.EvenDeeper": {20},
+            "func.InsideFunc": {38},
+        },
+    }
+
+
+@skip_pypy38
+def test_real_code_regions() -> None:
+    # Run code_regions on most of the coverage source code, checking that it
+    # succeeds and there are no overlaps.
+
+    cov_dir = Path(coverage.__file__).parent.parent
+    any_fails = False
+    # To run against all the files in the tox venvs:
+    #   for source_file in cov_dir.rglob("*.py"):
+    for sub in [".", "ci", "coverage", "lab", "tests"]:
+        for source_file in (cov_dir / sub).glob("*.py"):
+            regions = code_regions(source_file.read_text(encoding="utf-8"))
+            for kind, regs in regions.items():
+                line_counts = collections.Counter(lno for lnos in regs.values() for lno in lnos)
+                overlaps = [line for line, count in line_counts.items() if count > 1]
+                if overlaps:    # pragma: only failure
+                    print(
+                        f"{kind.title()} overlaps in {source_file.relative_to(Path.cwd())}: "
+                        + f"{overlaps}"
+                    )
+                    any_fails = True
+    if any_fails:
+        pytest.fail("Overlaps were found")  # pragma: only failure
