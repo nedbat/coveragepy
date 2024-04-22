2ff9933ee74a8d8048968fef9cdec68ba372b4dc
Ned Batchelder <ned@nedbatchelder.com>
2024-04-22 06:44:38 -0400

    docs: remove comment that now explains nothing.

--- coverage/__init__.py
+++ coverage/__init__.py
@@ -35,7 +35,3 @@ from coverage.plugin import (
 
 # Backward compatibility.
 coverage = Coverage
-
-# On Windows, we encode and decode deep enough that something goes wrong and
-# the encodings.utf_8 module is loaded and then unloaded, I don't know why.
-# Adding a reference here prevents it from being unloaded.  Yuk.
