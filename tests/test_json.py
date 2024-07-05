dba64067d3ff135dc37b60ad3e085c62b14e0cef
devdanzin <74280297+devdanzin@users.noreply.github.com>
2024-07-05 10:52:23 -0300

    feat: support regions (functions, classes) in JSON reports.

--- tests/test_json.py
+++ tests/test_json.py
@@ -27,8 +27,7 @@ class JsonReportTest(UsingModulesMixin, CoverageTest):
         expected_result: dict[str, Any],
     ) -> None:
         """
-        Helper that handles common ceremonies so tests can clearly show the
-        consequences of setting various arguments.
+        Helper that creates an example file for most tests.
         """
         self.make_file("a.py", """\
             a = {'b': 1}
@@ -41,9 +40,47 @@ class JsonReportTest(UsingModulesMixin, CoverageTest):
             if not a:
                 b = 4
             """)
-        a = self.start_import_stop(cov, "a")
-        output_path = os.path.join(self.temp_dir, "a.json")
-        cov.json_report(a, outfile=output_path)
+        self._compare_json_reports(cov, expected_result, "a")
+
+    def _assert_expected_json_report_with_regions(
+        self,
+        cov: Coverage,
+        expected_result: dict[str, Any],
+    ) -> None:
+        """
+        Helper that creates an example file for regions tests.
+        """
+        self.make_file("b.py", """\
+            a = {'b': 1}
+
+            def c():
+                return 1
+
+            class C:
+                pass
+
+            class D:
+                def e(self):
+                    return 2
+                def f(self):
+                    return 3
+            """)
+        self._compare_json_reports(cov, expected_result, "b")
+
+    def _compare_json_reports(
+        self,
+        cov: Coverage,
+        expected_result: dict[str, Any],
+        mod_name: str,
+    ) -> None:
+        """
+        Helper that handles common ceremonies, comparing JSON reports that
+        it creates to expected results, so tests can clearly show the
+        consequences of setting various arguments.
+        """
+        mod = self.start_import_stop(cov, mod_name)
+        output_path = os.path.join(self.temp_dir, f"{mod_name}.json")
+        cov.json_report(mod, outfile=output_path)
         with open(output_path) as result_file:
             parsed_result = json.load(result_file)
         self.assert_recent_datetime(
@@ -139,6 +176,108 @@ class JsonReportTest(UsingModulesMixin, CoverageTest):
             },
         }
         self._assert_expected_json_report(cov, expected_result)
+
+    def test_regions_coverage(self) -> None:
+        cov = coverage.Coverage()
+        expected_result = {
+            "meta": {
+                "branch_coverage": False,
+                "show_contexts": False
+            },
+            "files": {
+                "b.py": {
+                    "executed_lines": [1, 3, 6, 7, 9, 10, 12],
+                    "summary": {
+                        "covered_lines": 7,
+                        "num_statements": 10,
+                        "percent_covered": 70.0,
+                        "percent_covered_display": "70",
+                        "missing_lines": 3,
+                        "excluded_lines": 0
+                    },
+                    "missing_lines": [4, 11, 13],
+                    "excluded_lines": [],
+                    "function": {
+                        "c": {
+                            "executed_lines": [],
+                            "summary": {
+                                "covered_lines": 0,
+                                "num_statements": 1,
+                                "percent_covered": 0.0,
+                                "percent_covered_display": "0",
+                                "missing_lines": 1,
+                                "excluded_lines": 0
+                            },
+                            "missing_lines": [4],
+                            "excluded_lines": []
+                        },
+                        "D.e": {
+                            "executed_lines": [],
+                            "summary": {
+                                "covered_lines": 0,
+                                "num_statements": 1,
+                                "percent_covered": 0.0,
+                                "percent_covered_display": "0",
+                                "missing_lines": 1,
+                                "excluded_lines": 0
+                            },
+                            "missing_lines": [11],
+                            "excluded_lines": []
+                        },
+                        "D.f": {
+                            "executed_lines": [],
+                            "summary": {
+                                "covered_lines": 0,
+                                "num_statements": 1,
+                                "percent_covered": 0.0,
+                                "percent_covered_display": "0",
+                                "missing_lines": 1,
+                                "excluded_lines": 0
+                            },
+                            "missing_lines": [13],
+                            "excluded_lines": []
+                        }
+                    },
+                    "class": {
+                        "C": {
+                            "executed_lines": [],
+                            "summary": {
+                                "covered_lines": 0,
+                                "num_statements": 0,
+                                "percent_covered": 100.0,
+                                "percent_covered_display": "100",
+                                "missing_lines": 0,
+                                "excluded_lines": 0
+                            },
+                            "missing_lines": [],
+                            "excluded_lines": []
+                        },
+                        "D": {
+                            "executed_lines": [],
+                            "summary": {
+                                "covered_lines": 0,
+                                "num_statements": 2,
+                                "percent_covered": 0.0,
+                                "percent_covered_display": "0",
+                                "missing_lines": 2,
+                                "excluded_lines": 0
+                            },
+                            "missing_lines": [11, 13],
+                            "excluded_lines": []
+                        }
+                    }
+                }
+            },
+            "totals": {
+                "covered_lines": 7,
+                "num_statements": 10,
+                "percent_covered": 70.0,
+                "percent_covered_display": "70",
+                "missing_lines": 3,
+                "excluded_lines": 0
+            }
+        }
+        self._assert_expected_json_report_with_regions(cov, expected_result)
 
     def run_context_test(self, relative_files: bool) -> None:
         """A helper for two tests below."""
