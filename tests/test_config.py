9378e8551d760b9f120b6e917f2c7bf36de2ff13
Olena <107187316+OlenaYefymenko@users.noreply.github.com>
2025-04-18 23:59:10 +0300

    Add f-string for error message
    Co-authored-by: 🇺🇦 Sviatoslav Sydorenko (Святослав Сидоренко) <wk.cvs.github@sydorenko.org.ua>

--- tests/test_config.py
+++ tests/test_config.py
@@ -808,7 +808,7 @@ class ConfigFileTest(UsingModulesMixin, CoverageTest):
             xyzzy = 17
             """)
         with mock.patch.object(coverage.tomlconfig, "has_tomllib", False):
-            msg = "Can't read '{filename}' without TOML support"
+            msg = f"Can't read '{filename}' without TOML support"
             with pytest.raises(ConfigError, match=msg):
                 coverage.Coverage()
 
@@ -822,7 +822,7 @@ class ConfigFileTest(UsingModulesMixin, CoverageTest):
             run.parallel = true
             """)
         with mock.patch.object(coverage.tomlconfig, "has_tomllib", False):
-            msg = "Can't read '{filename}' without TOML support"
+            msg = f"Can't read '{filename}' without TOML support"
             with pytest.raises(ConfigError, match=msg):
                 coverage.Coverage()
 
